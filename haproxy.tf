7c1fc8b562a4b3b01d2f98b10ea57034da186b4f
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-14 09:39:14 +0200

    Update Haproxy - use native prometheus exporter

--- haproxy.tf
+++ haproxy.tf
@@ -4,24 +4,24 @@ resource "rancher_stack" "haproxy" {
   environment_id  = var.environment_id
   start_on_create = true
   finish_upgrade  = true
-  depends_on      = [rancher_secret.haproxy-config]
 
   docker_compose = templatefile("${path.module}/haproxy/docker-compose.yml",
-    { name   = var.name,
-      global = var.global,
-  affinity = var.affinity })
+    { global   = var.global,
+      affinity = var.affinity,
+      secret   = rancher_secret.haproxy-config.name })
   rancher_compose = templatefile("${path.module}/haproxy/rancher-compose.yml",
     { global   = var.global,
       user     = var.db_user,
       password = var.db_password,
       dbname   = var.db_name,
-  version = var.db_version })
+      version  = var.db_version })
 }
 
 resource rancher_secret "haproxy-config" {
   name           = "${var.name}-haproxy-config"
   environment_id = var.environment_id
-  value = templatefile("${path.module}/haproxy/haproxy.cfg", { fqdn = var.target_fqdn,
-    maxCount = var.target_maxCount,
-  port = var.target_port })
+  value          = templatefile("${path.module}/haproxy/haproxy.cfg",
+    { fqdn = var.target_fqdn,
+      maxCount = var.target_maxCount,
+      port = var.target_port })
 }
